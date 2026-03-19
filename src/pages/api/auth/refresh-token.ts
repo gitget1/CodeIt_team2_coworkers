d30f0e4a19400649d945f11dfc9a272516e5becd
unknown <jylee2670@gmail.com>
2026-03-19 17:03:43 +0900

    [COW-43] FEAT - backendFetcher 도입 및 에러 핸들링 통일

--- src/pages/api/auth/refresh-token.ts
+++ src/pages/api/auth/refresh-token.ts
@@ -1,29 +1,22 @@
 import { ACCESS_TOKEN_MAX_AGE, COOKIE_OPTIONS } from '@/shared/constants/auth';
-import axios, { isAxiosError } from 'axios';
+import backendFetcher from '@/shared/lib/axios/backend-fetcher';
+import { ApiError } from '@/shared/types/apiError';
 import { serialize } from 'cookie';
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-const BASE_URL = process.env.API_BASE_URL;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: '올바르지 않은 접근입니다' });
   }
 
-  if (!BASE_URL) {
-    return res.status(500).json({ message: 'API_BASE_URL이 정의되지 않았습니다' });
-  }
-
-  const refreshToken = req.cookies.refreshToken;
+  const { refreshToken } = req.cookies;
 
   if (!refreshToken) {
     return res.status(401).json({ message: 'Refresh Token이 없습니다' });
   }
 
   try {
-    const { data } = await axios.post<{ accessToken: string }>(`${BASE_URL}/auth/refresh-token`, {
-      refreshToken,
-    });
+    const { data } = await backendFetcher.post('/auth/refresh-token', { refreshToken });
 
     res.setHeader(
       'Set-Cookie',
@@ -35,12 +28,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     return res.status(200).json({ success: true, accessToken: data.accessToken });
   } catch (error: unknown) {
-    if (isAxiosError(error)) {
-      return res.status(error.response?.status || 401).json({
-        message: '올바르지 않은 refresh token입니다',
-      });
-    }
-
-    return res.status(500).json({ message: '서버 에러가 발생했습니다' });
+    const apiError = error as ApiError;
+    return res
+      .status(apiError.status || 500)
+      .json({ message: apiError.message || '요청 처리 중 서버 오류가 발생했습니다.' });
   }
 }
--- src/pages/api/auth/signUp.ts
+++ src/pages/api/auth/signUp.ts
@@ -3,26 +3,19 @@ import {
   COOKIE_OPTIONS,
   REFRESH_TOKEN_MAX_AGE,
 } from '@/shared/constants/auth';
+import backendFetcher from '@/shared/lib/axios/backend-fetcher';
 import { ApiError } from '@/shared/types/apiError';
-import axios, { isAxiosError } from 'axios';
 import { serialize } from 'cookie';
 import { NextApiRequest, NextApiResponse } from 'next';
-
-const BASE_URL = process.env.API_BASE_URL;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: '올바르지 않은 접근입니다.' });
   }
 
-  if (!BASE_URL) {
-    return res.status(500).json({ message: 'API_BASE_URL이 정의되지 않았습니다.' });
-  }
-
   try {
-    const response = await axios.post(`${BASE_URL}/auth/signUp`, req.body);
-
-    const { accessToken, refreshToken, user } = response.data;
+    const { data } = await backendFetcher.post('/auth/signUp', req.body);
+    const { accessToken, refreshToken, user } = data;
 
     res.setHeader('Set-Cookie', [
       serialize('accessToken', accessToken, {
@@ -37,13 +30,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     return res.status(200).json({ user });
   } catch (error: unknown) {
-    if (isAxiosError<ApiError>(error)) {
-      const status = error.response?.status || 500;
-      const message = error.response?.data?.message || '알 수 없는 에러가 발생했습니다.';
-
-      return res.status(status).json({ message });
-    }
-
-    return res.status(500).json({ message: '서버 내부 오류가 발생했습니다.' });
+    const apiError = error as ApiError;
+    return res
+      .status(apiError.status || 500)
+      .json({ message: apiError.message || '요청 처리 중 서버 오류가 발생했습니다.' });
   }
 }
