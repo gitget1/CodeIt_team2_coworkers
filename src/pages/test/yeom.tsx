652f78b78f7fba509aa5e49d0df17b8171609abd
YeomSeunghyeon <124681484+YeomSeunghyeon@users.noreply.github.com>
2026-03-19 17:12:41 +0900

    [COW-17] FEAT - skeleton 공통 컴포넌트
    * [COW-17] skeleton 공통 컴포넌트

--- src/pages/test/yeom.tsx
+++ src/pages/test/yeom.tsx
@@ -1,32 +1,12 @@
-import { useImagePicker } from '@/shared/hooks/useImagePicker';
-import { ImagePreviewList } from '@/shared/ui/imagePicker/ImagePreviewList';
-import { ImageUploadSlot } from '@/shared/ui/imagePicker/ImageUploadSlot';
+import { Skeleton } from "@/shared/ui/skeleton/Skeleton"
 
-export default function Test() {
-  const picker = useImagePicker({
-    maxCount: 5,
-    defaultImages: [
-      'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d',
-      'https://images.unsplash.com/photo-1492724441997-5dc865305da7',
-    ],
-  });
-
+export default function PostCardSkeleton() {
   return (
-    <div className="mt-12 flex justify-center gap-2">
-      <ImagePreviewList
-        images={picker.images}
-        onRemove={picker.removeImage}
-        className="h-[200px]"
-      />
+    <div className="space-y-3 rounded-lg border p-4">
+      <Skeleton className="h-6 w-2/3" />
 
-      <ImageUploadSlot
-        disabled={picker.images.length >= picker.maxCount}
-        onUpload={picker.addImages}
-        count={picker.images.length}
-        maxCount={picker.maxCount}
-        isCount={true}
-        className="h-[200px] w-[200px] rounded-xl bg-blue-50"
-      />
+      <Skeleton className="h-40 w-full rounded-md" />
     </div>
-  );
+
+  )
 }
