be9bbd9a80e41751bd5f68cf69d3a4eebdf18fef
unknown <jylee2670@gmail.com>
2026-03-19 23:47:53 +0900

    [COW-45] FEAT - 중복 로직 제거

--- src/features/auth/api/auth.service.ts
+++ src/features/auth/api/auth.service.ts
@@ -4,20 +4,21 @@ import { User } from '@/shared/types/user.model';
 import { mapUserDTOToDomain } from '../lib/mappers/auth.mapper';
 import { clientFetcher } from '@/shared/lib/axios/client-fetcher';
 
-export const authService = {
-  signIn: async (payload: SignInRequest): Promise<User> => {
-    const { data } = await axios.post<{ user: UserDTO }>('/api/auth/signIn', payload);
+const requestBffAuth = async (
+  endpoint: 'signIn' | 'signUp',
+  payload: SignInRequest | SignUpRequest,
+): Promise<User> => {
+  const { data } = await axios.post<{ user: UserDTO }>(`/api/auth/${endpoint}`, payload);
+  return mapUserDTOToDomain(data.user);
+};
 
-    return mapUserDTOToDomain(data.user);
-  },
+export const authService = {
+  signIn: (payload: SignInRequest) => requestBffAuth('signIn', payload),
+
+  signUp: (payload: SignUpRequest) => requestBffAuth('signUp', payload),
 
   signOut: async (): Promise<void> => {
     await axios.post('/api/auth/signOut');
-  },
-
-  signUp: async (payload: SignUpRequest): Promise<User> => {
-    const { data } = await axios.post<{ user: UserDTO }>('/api/auth/signUp', payload);
-    return mapUserDTOToDomain(data.user);
   },
 
   getUserMe: async (): Promise<User> => {
