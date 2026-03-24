7ac0121d2dfbcfbbd7bf80cb0ebe4296adb95c80
unknown <jylee2670@gmail.com>
2026-03-24 16:37:40 +0900

    [COW-74] FEAT - 회원가입 페이지 구현

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,22 @@
+import { SignUpForm } from '@/features/auth/ui/SignUpForm';
+import logoLg from '@/shared/assets/images/logo-lg.png';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function LoginPage() {
+  return (
+    <>
+      <main className="bg-background-secondary flex min-h-screen flex-col items-center justify-center px-4">
+        <div className="w-full max-w-120">
+          <header className="mb-8 flex justify-center">
+            <Link href="/">
+              <Image src={logoLg} alt="Coworkers Logo" className="object-contain" priority />
+            </Link>
+          </header>
+
+          <SignUpForm />
+        </div>
+      </main>
+    </>
+  );
+}
