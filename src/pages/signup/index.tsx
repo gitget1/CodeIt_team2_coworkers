9b56331c772ba79921cb6b33bdc7da40503bcccc
unknown <jylee2670@gmail.com>
2026-03-24 18:56:04 +0900

    [COW-74] REFACTOR - signup page 오타 수정

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,7 +3,7 @@ import logoLg from '@/shared/assets/images/logo-lg.png';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function LoginPage() {
+export default function SignUpPage() {
   return (
     <>
       <main className="bg-background-secondary flex min-h-screen flex-col items-center justify-center px-4">
