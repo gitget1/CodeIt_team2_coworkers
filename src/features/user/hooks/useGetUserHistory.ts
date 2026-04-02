8b782ce85eece2e9d672951350eaae9aec51c9a8
summerr-min <alsgk7118@gmail.com>
2026-04-02 15:59:25 +0900

    [COW-126] FIX - 비로그인 시 히스토리 api 호출 방지 수정 (#124)
    Co-authored-by: minha <luck7118@naver.com>

--- src/features/user/hooks/useGetUserHistory.ts
+++ src/features/user/hooks/useGetUserHistory.ts
@@ -5,11 +5,15 @@ import { ApiError } from '@/shared/types/apiError';
 import { USER_QUERY_KEYS } from '../lib/queryKeys';
 import { toUserTaskHistory } from '../lib/mappers/user.mapper';
 import { UserTaskHistory } from '../model/entities/user.model';
+import { useUserQuery } from './useUserQuery';
 
 export const useGetUserHistory = () => {
+  const { data: user } = useUserQuery();
+
   return useQuery<GetUserHistoryResponse, ApiError, UserTaskHistory[]>({
     queryKey: USER_QUERY_KEYS.history(),
     queryFn: getUserHistory,
     select: (data) => data.tasksDone.map(toUserTaskHistory),
+    enabled: !!user,
   });
 };
